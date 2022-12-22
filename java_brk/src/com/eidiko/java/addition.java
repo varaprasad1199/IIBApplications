aed1bc607b3f691a8b61383cc238817cc07464cf
Sreenivas Bandaru <varaprasad.k39@gmail.com>
2022-12-22 17:42:14 +0530

    log4J

---
+++ java_brk/src/com/eidiko/java/addition.java
@@ -0,0 +1,14 @@
+package com.eidiko.java;
+
+public class addition {
+
+	public static String add(String a, String b) {
+		// TODO Auto-generated method stub
+        // String c = "PREETHI"+"REDDY";
+		String c = a + b;
+        return  c;
+        
+	}
+
+}
+
